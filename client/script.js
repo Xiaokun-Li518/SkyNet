09db3d0ad43eeb5d654337af6802a75a1fc5af41
li,xiaokun <leeli@iu.edu>
2023-03-07 19:40:09 -0500

    delete apikey

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const API_KEY = "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";
+const API_KEY = "";
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
